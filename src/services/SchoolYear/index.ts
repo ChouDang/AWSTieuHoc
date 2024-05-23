ca5a6c0e7ade132b326bc6a40bb60a58add97e61
ChouDang <hoaitrongdang@gmail.com>
2024-05-23 11:24:17 +0700

    fix api

--- src/services/SchoolYear/index.ts
+++ src/services/SchoolYear/index.ts
@@ -5,5 +5,10 @@ import { SchoolYear } from '../../consts/types'
 const client = generateClient<Schema>()
 
 export const createTodo = async (_data: SchoolYear) => {
-    await client.models.SchoolYear.create(_data)
+    await client.models.SchoolYear.create({
+        YearName: "2024 - 2025",
+        Start: new Date().toUTCString() ,
+        End: new Date().toUTCString(),
+        Status: "Active",
+    })
 }
